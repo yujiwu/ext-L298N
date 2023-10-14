eac83fbd8809b1241da3da3ebc123ca6044e3a00
Yujie Wu <yujiewu@YujiedeiMac.local>
2023-10-14 17:00:11 +0800

    10/14/23 First Commit

--- README.md
+++ README.md
@@ -1,6 +1,59 @@
-# MPU-6050 module
-* MPU-6050 module uses I2C to communicate with arduino
-* It requires 2 wires to connect to A4(SDA) and A5(SCL) pins on arduino
+# L298N module
+* L298N module uses 2 pins to control each motor; 2 motors maximium
+* pin VIN and GND on the board accept 4.5V-36V external power supply
+* connect pin IN1, IN2, IN3, IN4 to pin 2,3,4,5 on Arduino
+* connect pin EN1, EN2 to pin 9,10 (with analog mode) on Arduino; or without EN1, EN2 with jumper in place
+* connect pin +5V, GND to pin Vcc and GND on Arduino to power it up (it requires 12V maximium voltage connects to VIN)
+* if the input voltage over 12V connecting to the VIN, it will burn the on-board bec, so remove the jumper before connection; and power up the l298n by using the +5V pin 
+
+# Notice
+1、共地问题
+L298N 通常需要使用单片机提供 PWM 信号作为输入调速信号，具体怎么实现的懂电路的同学可以把原理图找来看看，虽然有很多版本，但是基本都是大同小异。
+做软件的同学需要注意的是：PWM 的占空比与电压之间的换算关系，
+另外有一条最需要注意的就是PWM 不能直接控制电机转动！！！！！！！！！别把PWM直接接电机！！！！！
+
+原因分析：
+通常这种行为是用 Arduino 板子时出现的，因为这东西可以 USB 供电，所以很多人直接在线调试，调个GPIO（小灯）还可以，但是调电机的时候用电池给L298N供电。
+
+共地要求单片机的地（GND）和电机（motors，任意1根线，非必要）、L298N 的GND、直流电源（GND）是同一个。
+
+现象
+
+当不满足上述条件时可能出现的现象（自己遇到过的）：
+
+（1）电机不转；
+
+（2）输出端口电压 0V；
+
+（3）电笔或其他导体（包括手）触碰输出端子，电机转动（可能是静电导致）。
+
+解决方案：
+
+自己想办法使两个共地吧，不要简单地把两个GND连在一起~！
+
+2、功率不足
+有些人可能了解共地的重要性，但是明明线连的都对，为什么电机还是不转呢？
+
+出现原因
+
+这时候你就要考虑整体功率不足的问题了，因为L298N本身需要一定的电能（忘记在哪看到的），所以在驱动电机的时候，5V电压根本带不动电机，表面 5V 的电压，电流可能远不达标。
+
+现象：
+
+出现这种问题的通常是因为单片机供电 5V 左右就可以了，而L298N也可以5V供电，为了方便，当然是单电源双用，这时会出现以下现象（自己遇到过的）：
+
+（1）电机不转；
+
+（2）带负载时电机输出端子电压很小；
+
+（3）卸掉负载电机后电压正常；
+
+（4）指示灯（L1,L2,L3,L4）按程序正常闪烁。
+
+解决方案：
+
+选取12V电源，对L298N和单片机供电，单片机如果需要5V的话，还需要接一个稳压模块或自己做降压（不推荐，烧过一块板子，伤心）。
+
 
 # Block Description
 * Use the first block to initialize the module
--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -1,162 +1,56 @@
 // define the enumerator for dropdown menu on the block
-// enum SIZE {1,2}
 
-//% color="#52c3f0" iconWidth=40 iconHeight=40
-namespace mpu6050 {
-    //% block="initialize the module" blockType="command"
-    export function moduleInit(parameter: any, block: any){
-        Generator.addInclude('Adafruit_MPU6050', '#include <Adafruit_MPU6050.h>', true);
-        Generator.addInclude('Adafruit_Sensor', '#include <Adafruit_Sensor.h>', true);
-        Generator.addInclude('Wire', '#include <Wire.h>', true);
-        Generator.addInclude('Math', '#include <Math.h>', true);
-        Generator.addObject('mpu', 'Adafruit_MPU6050', 'mpu;', true);
-        Generator.addObject('errorVariables', 'float', 'accXError, accYError, accZError, gyroXError, gyroYError, gyroZError, tempError;', true);
-        Generator.addObject('rad2Deg', 'float', 'rad2Deg = 57.295779513f;', true);
-        Generator.addSetup('startSerial', 'Serial.begin(9600);\n\t'+
-        'while (!Serial){\n\t'+
-        '  delay(100); // will pause Zero, Leonardo, etc until serial console opens\n\t'+
-        '}\n\t'+
-        'Serial.println("Adafruit MPU6050 test!");\n', true);
+enum motors {
+    M1, M2, M3, M4, M5, M6
+}
 
-        Generator.addSetup('initialize', '// Try to initialize!\n\t'+
-        'if (!mpu.begin()) {\n\t'+
-        '  Serial.println("Failed to find MPU6050 chip");\n\t'+
-        '  while (1) {\n\t'+
-        '      delay(10);\n\t'+
-        '  }\n\t'+
-        '}\n\t'+
-        'Serial.println("MPU6050 Found!");\n', true);
-        
-        Generator.addSetup('setupAcc', '// set accelerometer unit, in this case -8g to +8g\n\t'+ 
-        'mpu.setAccelerometerRange(MPU6050_RANGE_8_G);\n\t'+
-        'Serial.print("Accelerometer range set to: ");\n\t'+
-        'switch (mpu.getAccelerometerRange()) {\n\t'+
-        'case MPU6050_RANGE_2_G:\n\t'+
-        '  Serial.println("+-2G");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_4_G:\n\t'+
-        '  Serial.println("+-4G");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_8_G:\n\t'+
-        '  Serial.println("+-8G");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_16_G:\n\t'+
-        '  Serial.println("+-16G");\n\t'+
-        '  break;\n\t'+
-        '}\n', true);
-        
-        Generator.addSetup('setupGyro', '// set gyro unit, in this case 500 degree/s\n\t'+
-        'mpu.setGyroRange(MPU6050_RANGE_500_DEG);\n\t'+
-        'Serial.print("Gyro range set to: ");\n\t'+
-        'switch (mpu.getGyroRange()) {\n\t'+
-        'case MPU6050_RANGE_250_DEG:\n\t'+
-        '  Serial.println("+- 250 deg/s");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_500_DEG:\n\t'+
-        '  Serial.println("+- 500 deg/s");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_1000_DEG:\n\t'+
-        '  Serial.println("+- 1000 deg/s");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_RANGE_2000_DEG:\n\t'+
-        '  Serial.println("+- 2000 deg/s");\n\t'+
-        '  break;\n\t'+
-        '}\n', true);
+enum pins {
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+}
 
-        Generator.addSetup('setupBandwidth', '// set filter bandwidth\n\t'+
-        'mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);\n\t'+
-        'Serial.print("Filter bandwidth set to: ");\n\t'+
-        'switch (mpu.getFilterBandwidth()) {\n\t'+
-        'case MPU6050_BAND_260_HZ:\n\t'+
-        '  Serial.println("260 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_184_HZ:\n\t'+
-        '  Serial.println("184 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_94_HZ:\n\t'+
-        '  Serial.println("94 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_44_HZ:\n\t'+
-        '  Serial.println("44 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_21_HZ:\n\t'+
-        '  Serial.println("21 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_10_HZ:\n\t'+
-        '  Serial.println("10 Hz");\n\t'+
-        '  break;\n\t'+
-        'case MPU6050_BAND_5_HZ:\n\t'+
-        '  Serial.println("5 Hz");\n\t'+
-        '  break;\n\t'+
-        '}\n\t'+
-        'Serial.println("");\n', true);
+enum actions {
+    Forward, Backward, Stop
+}
 
-        Generator.addSetup('calibration', '//calibrate mpu6050\n\t'+
-        '// request data\n\t'+
-        'float accXSum, accYSum, accZSum, gyroXSum, gyroYSum, gyroZSum, tempSum = 0;\n\t'+
-        'sensors_event_t a, g, temp;\n\t'+
-        'mpu.getEvent(&a, &g, &temp);\n\t'+
-        
-        'for (int i = 0; i < 1000; i++){\n\t'+
-        '  accXSum += a.acceleration.x;\n\t'+
-        '  accYSum += a.acceleration.y;\n\t'+
-        '  accZSum += a.acceleration.z;\n\t'+
-        '  tempSum += temp.temperature;\n\t'+
-        '  gyroXSum += g.gyro.x;\n\t'+
-        '  gyroYSum += g.gyro.y;\n\t'+
-        '  gyroZSum += g.gyro.z;\n\t'+
-        '  delay(10); // read data every 10 ms\n\t'+
-        '}\n\t'+
-
-        'accXError = accXSum / 1000;\n\t'+
-        'accYError = accYSum / 1000;\n\t'+
-        'accZError = accZSum / 1000 - 9.81;\n\t'+
-        'gyroXError = gyroXSum / 1000;\n\t'+
-        'gyroYError = gyroYSum / 1000;\n\t'+
-        'gyroZError = gyroZSum / 1000;\n\t'+
-        'tempError = tempSum / 1000;\n\t'+
-        'delay(100);\n', true);
+//% color="#e81f1c" iconWidth=40 iconHeight=40
+namespace l298n {
+    //% block="initialize the motor [LIST] with pin1 [PIN1] and pin2 [PIN2]" blockType="command"
+    //% LIST.shadow="dropdown" LIST.options="motors" LIST.defl="motors.M1"
+    //% PIN1.shadow="dropdown" PIN1.options="pins" PIN1.defl="pins.2"
+    //% PIN2.shadow="dropdown" PIN2.options="pins" PIN2.defl="pins.3"
+    export function motorInit(parameter: any, block: any){
+        let list = parameter.LIST.code; 
+        let pin1 = parameter.PIN1.code;
+        let pin2 = parameter.PIN2.code;
+        Generator.addInclude('L298N', '#include <L298N.h>', true);
+        Generator.addObject('Motors', 'L298N', `${list}(${pin1}, ${pin2});`);
     }
 
-    //% block="get original data" blockType="command"
-    export function getOriginalData(){
-        Generator.addCode('// Get new sensor events with the readings\n\t'+
-        'sensors_event_t a, g, temp;\n\t'+
-        'mpu.getEvent(&a, &g, &temp);\n\t'+
-        '// calibrate the data and store in the variables\n\t'+
-        'float accX = a.acceleration.x - accXError;\n\t'+
-        'float accY =  a.acceleration.y - accYError;\n\t'+
-        'float accZ = a.acceleration.z - accYError;\n\t'+
-        'float gyroX = g.gyro.x - gyroXError;\n\t'+
-        'float gyroY = g.gyro.y - gyroYError;\n\t'+
-        'float gyroZ = g.gyro.z - gyroZError;\n\t'+
-        'float mpuTemp = temp.temperature - tempError;\n');
+    //% block="motor [LIST] [ACTION]" blockType="command"
+    //% LIST.shadow="dropdown" LIST.options="motors" LIST.defl="motors.M1"
+    //% ACTION.shadow="dropdown" ACTION.options="actions" ACTION.defl="actions.Forward"
+    export function motorAction(parameter: any, block: any){
+        let action = parameter.ACTION.code;
+        let list = parameter.LIST.code;
+        switch (action){
+            case 'Forward': 
+                Generator.addCode(`${list}.forward();`);
+                break;
+            case 'Backward':
+                Generator.addCode(`${list}.backward();`);
+                break;
+            case 'Stop':
+                Generator.addCode(`${list}.stop();`);
+                break;
+        }
     }
 
-    //% block="get roll" blockType="reporter"
-    export function getRoll(){
-        Generator.addObject('getRoll', 'float', 'getRoll(float accX, float accY, float accZ){\n\t'+
-        'float mag = sqrt(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2));\n\t'+
-        'float magXZ = sqrt(pow(accX, 2) + pow(accZ, 2));\n\t'+
-        '// direction of accY, positive or negative\n\t'+
-        'int accYDir = accY/abs(accY);\n\t'+
-        'int accZDir = accZ/abs(accZ);\n\t'+
-        'return accYDir * acos(accZDir * magXZ / mag) * rad2Deg;\n\t'+
-        '}\n', true);
-        Generator.addCode('getRoll(accX, accY, accZ)');
+    //% block="set the motor [LIST] speed [SPEED]" blockType="command"
+    //% LIST.shadow="dropdown" LIST.options="motors" LIST.defl="motors.M1"
+    //% SPEED.shadow="range" SPEED.params.min=0 SPEED.params.max=255 SPEED.defl=255
+    export function setMotorSpeed(parameter: any, block: any){
+        let list = parameter.LIST.code;
+        let speed = parameter.SPEED.code;
+        Generator.addCode(`${list}.setSpeed(${speed});`);
     }
-
-    //% block="get pitch" blockType="reporter"
-    export function getPitch(){
-        Generator.addObject('getPitch', 'float', 'getPitch(float accX, float accY, float accZ){\n\t'+
-        'float mag = sqrt(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2));\n\t'+
-        'float magYZ = sqrt(pow(accY, 2) + pow(accZ, 2));\n\t'+
-        '// direction of accX positive or negative\n\t'+
-        'int accXDir = accX/abs(accX);\n\t'+
-        'int accZDir = accZ/abs(accZ);\n\t'+
-        'return accXDir * acos(accZDir * magYZ / mag) * rad2Deg;\n\t'+
-        '}\n', true);
-        Generator.addCode('getPitch(accX, accY, accZ)');
-    }
-
 }
